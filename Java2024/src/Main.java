c21e381b204d64b396e752cd06347cf3bcccad4e
manic-066g <xeungjin@nate.com>
2024-04-03 17:33:45 +0900

    2024-04-03

--- Java2024/src/Main.java
+++ Java2024/src/Main.java
@@ -1,121 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      Point1[] pt1 = new Point1[2];
-      Point2[] pt2 = new Point2[2];
-      Point3[] pt3 = new Point3[2];
-      
-      for (int i= 0; i < pt1.length; i++) {
-         int x1 = sc.nextInt();
-         int y1 = sc.nextInt();
-         pt1[i] = new Point1(x1, y1);
-      }   
-         
-         
-      for (int i= 0; i < pt2.length; i++) {
-         int x2 = sc.nextInt();
-         int y2 = sc.nextInt();
-         pt2[i] = new Point2(x2, y2);
-      }   
-         
-      
-      for (int i= 0; i < pt3.length; i++) {      
-         int x3 = sc.nextInt();
-         int y3 = sc.nextInt();
-         pt3[i] = new Point3(x3, y3);
-      }   
-         sc.close();
-         
-         for (int i = 0; i < pt1.length; i++) {
-            pt1[i].print();
-         
-      }
-         for (int i = 0; i < pt2.length; i++) {
-            pt2[i].print();
-         
-      }
-         for (int i = 0; i < pt3.length; i++) {
-            pt3[i].print();
-         
-      }
-         
-      int sumLocation1 = 0, sumLocation = 0, sumLocation3 = 0;
-      for (int = 0; i < pt; )
-      
-      System.out.println(pt1);
-      System.out.println(pt2);
-      System.out.println(pt3);
-   }
-}
+		// TODO Auto-generated method stub
 
-class Point1 {
-	private int x1;
-	private int y1;
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		sc.close();
 
-	public Point1(int x1, int y1) {
-		this.x1 = x1;
-		this.y1 = y1;
-	}
+		String[] values = input.split("\\s+");
 
-	public void print() {
-		System.out.println(x1 + " " + y1);
-	}
+		int h = Integer.parseInt(values[0]) * 60;
+		int m = Integer.parseInt(values[1]);
 
-	public int getX1() {
-		return x1;
-	}
+		int sum = h + m - 45;
 
-	public int getY1() {
-		return y1;
-	}
+		int day = 24 * 60;
+		if (sum >= day)
+			sum -= day;
 
-}
+		else if (sum < 0)
+			sum += day;
 
-class Point2 {
+		int result_m = sum % 60;
+		int result_h = sum / 60;
 
-	private int x2;
-	private int y2;
-
-	public Point2(int x2, int y2) {
-		this.x2 = x2;
-		this.y2 = y2;
-	}
-
-	public void print() {
-		System.out.println(x2 + " " + y2);
-	}
-
-	public int getX2() {
-		return x2;
-	}
-
-	public int getY2() {
-		return y2;
-	}
-}
-
-class Point3 {
-
-	private int x3;
-	private int y3;
-
-	public Point3(int x3, int y3) {
-		this.x3 = x3;
-		this.y3 = y3;
-	}
-
-	public void print() {
-		System.out.println(x3 + " " + y3);
-	}
-
-	public int getX3() {
-		return x3;
-	}
-
-	public int getY3() {
-		return y3;
+		System.out.println(result_h + " " + result_m);
 	}
 }
