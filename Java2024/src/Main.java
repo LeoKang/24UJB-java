501ef66ef484d5d6fcd063d4c86390f311502d3a
manic-066g <xeungjin@nate.com>
2024-04-04 12:56:11 +0900

    2024-04-04

--- Java2024/src/Main.java
+++ Java2024/src/Main.java
@@ -2,29 +2,30 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		Scanner sc = new Scanner(System.in);
-		String input = sc.nextLine();
+		int H = sc.nextInt();
+		int M = sc.nextInt();
+		int T = sc.nextInt();
+		int S = (M + T);
 		sc.close();
 
-		String[] values = input.split("\\s+");
+		for (;;) {
+			if (S >= 60) {
+				H += 1;
+				S -= 60;
+				if (H >= 24) {
+					H -= 24;
+				}
 
-		int h = Integer.parseInt(values[0]) * 60;
-		int m = Integer.parseInt(values[1]);
-
-		int sum = h + m - 45;
-
-		int day = 24 * 60;
-		if (sum >= day)
-			sum -= day;
-
-		else if (sum < 0)
-			sum += day;
-
-		int result_m = sum % 60;
-		int result_h = sum / 60;
-
-		System.out.println(result_h + " " + result_m);
+				// System.out.print(H + " ");
+				// System.out.println(S);
+				if (S <= 60) {
+					System.out.print(H + " ");
+					System.out.println(S);
+				}
+			}else {
+				break;
+			}
+		}
 	}
 }
