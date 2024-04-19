21bbd81b79d1fa60d2904cf49a4f356dacc1c6d1
manic-066g <xeungjin@nate.com>
2024-04-19 12:46:18 +0900

    DB connection , insert

--- Java2024/src/Main.java
+++ Java2024/src/Main.java
@@ -1,23 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		double sum = 0;
-		double avg;
-		int cnt = 0;
-		for (;;) {
-			int A = sc.nextInt();
-			if (0 <= A && A <= 100) {
-				sum += A;
-				cnt++;
-			} else {
-//				sc.close();
-				avg = (sum / cnt);
-				System.out.println("sum : " + (int) sum);
-				System.out.print("avg : ");
-				System.out.printf("%.1f", avg);
-			}
+		String str = sc.next();
+		sc.close();
+		for (int i = 0; i < str.length(); i++) {
+			char anw = str.charAt(str.length() - 1);
+			String name = anw + str.substring(0, str.length() - 1);
+			str = name;
+			System.out.println(name);
 		}
 	}
 }
