34cb97938a238fdba4f8210ef241b7e8ed585137
manic-066g <xeungjin@nate.com>
2024-04-22 16:35:34 +0900

    콘솔 게시판 구현(삽입, 조회)

--- Java2024/src/Main.java
+++ Java2024/src/Main.java
@@ -1,17 +1,30 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		String str = sc.next();
+		int size = sc.nextInt();
+		int num = sc.nextInt();
+		int[] arr = new int[size];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
 		sc.close();
-		for (int i = 0; i < str.length(); i++) {
-			char anw = str.charAt(str.length() - 1);
-			String name = anw + str.substring(0, str.length() - 1);
-			str = name;
-			System.out.println(name);
+		bfs(arr, num);
+	}
+
+	public static void bfs(int[] arr, int num) {
+		Queue<Integer> queue = new ArrayDeque<>();
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] < num) {
+				queue.offer(arr[i]);
+			}
+		}
+		while (!queue.isEmpty()) {
+			System.out.print(queue.poll() + " ");
 		}
 	}
 }
