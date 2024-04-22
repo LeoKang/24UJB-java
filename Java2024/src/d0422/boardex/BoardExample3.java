d1413a421f2302118d226d0314bfdba9f133734a
manic-066g <xeungjin@nate.com>
2024-04-22 16:44:44 +0900

    게시물 읽기 기능

--- Java2024/src/d0422/boardex/BoardExample3.java
+++ Java2024/src/d0422/boardex/BoardExample3.java
@@ -80,7 +80,41 @@ public class BoardExample3 {
 	}
 
 	private void read() {
-		System.out.println("*** read() 메소드 실행됨");
+		System.out.println("[게시물 읽기]");
+		System.out.print("bno : ");
+		int bno = Integer.parseInt(scanner.nextLine());
+
+		try {
+			String sql = ""
+					+ "SELECT bno, btitle, bcontent, bwriter, bdate "
+					+ "FROM boards "
+					+ "WHERE bno=?";
+
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, bno);
+			ResultSet rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				Board board = new Board();
+				board.setBno(rs.getInt("bno"));
+				board.setBtitle(rs.getString("btitle"));
+				board.setBcontent(rs.getString("bcontent"));
+				board.setBwriter(rs.getString("bwriter"));
+				board.setBdate(rs.getDate("bdate"));
+				System.out.println("##############");
+				System.out.println("번호 : " + board.getBno());
+				System.out.println("제목 : " + board.getBtitle());
+				System.out.println("내용 : " + board.getBcontent());
+				System.out.println("작성자 : " + board.getBwriter());
+				System.out.println("날짜 : " + board.getBdate());
+				System.out.println("##############");
+			}
+			rs.close();
+			pstmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			exit();
+		}
 		list();
 	}
 
@@ -101,8 +135,7 @@ public class BoardExample3 {
 		String menuNo = scanner.nextLine();
 		if (menuNo.equals("1")) {
 			try {
-				String sql = ""
-						+ "INSERT INTO boards (bno, btitle, bcontent, bwriter, bdate) "
+				String sql = "" + "INSERT INTO boards (bno, btitle, bcontent, bwriter, bdate) "
 						+ "VALUES (SEQ_BNO.NEXTVAL, ?, ?, ?, SYSDATE)";
 
 				PreparedStatement pstmt = conn.prepareStatement(sql);
